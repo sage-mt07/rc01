f63dd37d00a5e4dd2fe31e5d64555da64bc0a315
seijim <seiji7m@gmail.com>
2025-07-21 18:02:42 +0900

    up

--- tests/Core/CoreExceptionsTests.cs
+++ tests/Core/CoreExceptionsTests.cs
@@ -30,26 +30,5 @@ public class CoreExceptionsTests
         Assert.Equal("msg", ex.Message);
     }
 
-    [Fact]
-    public void CoreValidationException_StoresErrors()
-    {
-        var errs = new List<string> { "a", "b" };
-        var ex = new CoreValidationException("m", errs);
-        Assert.Equal(errs, ex.ValidationErrors);
-        var ex2 = new CoreValidationException(errs);
-        Assert.Equal(2, ex2.ValidationErrors.Count);
-        Assert.Contains("Validation failed", ex2.Message);
-    }
 
-    [Fact]
-    public void EntityModelException_Constructors()
-    {
-        var ex1 = new EntityModelException(typeof(string), "bad");
-        Assert.Equal(typeof(string), ex1.EntityType);
-        Assert.Contains("String", ex1.Message);
-        var inner = new Exception("i");
-        var ex2 = new EntityModelException(typeof(int), "x", inner);
-        Assert.Equal(inner, ex2.InnerException);
-        Assert.Equal(typeof(int), ex2.EntityType);
-    }
 }
