3c467b6b60b4c4dd57914dbc889ab779448357ca
sage-mt07 <65053888+sage-mt07@users.noreply.github.com>
2025-07-23 22:30:06 +0900

    Clarify connectivity test steps

---
+++ physicalTests/Connectivity/PortConnectivityTests.cs
@@ -0,0 +1,39 @@
+using Confluent.Kafka;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Kafka.Ksql.Linq.Application;
+using Xunit;
+
+namespace Kafka.Ksql.Linq.Tests.Integration;
+
+[TestCaseOrderer("Kafka.Ksql.Linq.Tests.Integration.PriorityOrderer", "Kafka.Ksql.Linq.Tests.Integration")]
+public class PortConnectivityTests
+{
+[Fact]
+[TestPriority(1)]
+    public void Kafka_Broker_Should_Be_Reachable()
+    {
+        using var admin = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = "localhost:9092" }).Build();
+        var meta = admin.GetMetadata(TimeSpan.FromSeconds(10));
+        Assert.NotEmpty(meta.Brokers);
+    }
+
+[Fact]
+[TestPriority(2)]
+    public async Task SchemaRegistry_Should_Be_Reachable()
+    {
+        using var http = new HttpClient();
+        var resp = await http.GetAsync("http://localhost:8081/subjects");
+        Assert.True(resp.IsSuccessStatusCode);
+    }
+
+[Fact]
+[TestPriority(3)]
+    public async Task KsqlDb_Should_Be_Reachable()
+    {
+        await using var ctx = TestEnvironment.CreateContext();
+        var result = await ctx.ExecuteStatementAsync("SHOW TOPICS;");
+        Assert.True(result.IsSuccess);
+    }
+}
