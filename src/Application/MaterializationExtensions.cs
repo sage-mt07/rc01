92d525d69827dcdea43c4d21af199a0fb72712d6
sage-mt07 <65053888+sage-mt07@users.noreply.github.com>
2025-07-24 17:35:07 +0900

    Add schema materialization utility

---
+++ src/Application/MaterializationExtensions.cs
@@ -0,0 +1,28 @@
+using Kafka.Ksql.Linq.Core.Models;
+using Kafka.Ksql.Linq.Messaging.Internal;
+using Kafka.Ksql.Linq.SchemaRegistryTools;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+
+namespace Kafka.Ksql.Linq.Application;
+
+public static class MaterializationExtensions
+{
+    public static async Task EnsureMaterializedIfSchemaIsNewAsync<T>(this KsqlContext context) where T : class, new()
+    {
+        if (context == null) throw new ArgumentNullException(nameof(context));
+        var client = context.GetSchemaRegistryClient();
+        var model = context.GetEntityModels()[typeof(T)];
+        var ns = typeof(T).Namespace?.ToLowerInvariant() ?? string.Empty;
+        var name = typeof(T).Name.ToLowerInvariant();
+        var subject = $"{ns}.{name}-value";
+        var schema = DynamicSchemaGenerator.GetSchemaJson<T>();
+        var result = await client.RegisterSchemaIfNewAsync(subject, schema);
+        if (result.WasCreated)
+        {
+            var dummy = DummyObjectFactory.CreateDummy<T>();
+            await context.Set<T>().AddAsync(dummy, new Dictionary<string, string> { ["is_dummy"] = "true" });
+        }
+    }
+}
---
+++ tests/StubEntitySet.cs
@@ -0,0 +1,26 @@
+using Kafka.Ksql.Linq.Core.Abstractions;
+using Kafka.Ksql.Linq.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Kafka.Ksql.Linq.Tests;
+
+internal class StubEntitySet<T> : IEntitySet<T> where T : class
+{
+    public List<(T Entity, Dictionary<string,string>? Headers)> Added { get; } = new();
+    public Task AddAsync(T entity, Dictionary<string, string>? headers = null, CancellationToken cancellationToken = default)
+    {
+        Added.Add((entity, headers));
+        return Task.CompletedTask;
+    }
+    public Task RemoveAsync(T entity, CancellationToken cancellationToken = default) => Task.CompletedTask;
+    public Task<List<T>> ToListAsync(CancellationToken cancellationToken = default) => Task.FromResult(new List<T>());
+    public Task ForEachAsync(Func<T, Task> action, TimeSpan timeout = default, CancellationToken cancellationToken = default) => Task.CompletedTask;
+    public Task ForEachAsync(Func<T, KafkaMessageContext, Task> action, TimeSpan timeout = default, CancellationToken cancellationToken = default) => Task.CompletedTask;
+    public string GetTopicName() => "stub";
+    public EntityModel GetEntityModel() => new EntityModel { EntityType = typeof(T) };
+    public IKsqlContext GetContext() => throw new NotImplementedException();
+    public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default) => throw new NotImplementedException();
+}
